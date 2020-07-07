14cb2306092eb334f2df4c3070b13f0aa1b03c9c
Terry Abraham <terry-ab>
2020-07-07 22:32:06 +0530

    update

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ from sklearn import preprocessing
 import tensorflow as tf
 from tensorflow.python.keras.backend import set_session
 import pickle
+import os
 
+Image_folder=os.path.join('static','icons')
 app=Flask(__name__)
+app.config['UPLOAD_FOLDER']=Image_folder
 graph = tf.get_default_graph()
 sess = tf.Session()
 set_session(sess)
@@ -35,8 +38,22 @@ def predict():
         set_session(sess)
         pre=model.predict_classes(pad_sequences(tokenizer.texts_to_sequences(newtext),maxlen=maxlen,padding='post'))
     result=le.inverse_transform(pre)[0]
-
-    return render_template('index.html',prediction_text='Predicted class is :   {}'.format(result))
+    if result=="business":
+        img_file=os.path.join(app.config['UPLOAD_FOLDER'],'marketing.svg')
+        img_name="Business"
+    elif result=='entertainment':
+        img_file=os.path.join(app.config['UPLOAD_FOLDER'],'entertain.svg')
+        img_name="Entertainment"
+    elif result=='politics':
+        img_file=os.path.join(app.config['UPLOAD_FOLDER'],'politics.svg')
+        img_name="Politics"
+    elif result=='tech':
+        img_file=os.path.join(app.config['UPLOAD_FOLDER'],'tech.svg')
+        img_name="Technology"
+    else:
+        img_file=os.path.join(app.config['UPLOAD_FOLDER'],'sport.svg')
+        img_name="Sports"
+    return render_template('index.html',img_file=img_file,img_name=img_name)
 
 if __name__=="__main__":
     app.run(debug=True)
